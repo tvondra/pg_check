4eefdce8c9bad0cc5d986a0e6f5ec8f88093bb0b
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2011-11-11 14:08:31 +0200

    Remove a few bogus copy-pasted file header comments.

--- uninstall_pg_check.sql
+++ uninstall_pg_check.sql
@@ -1,5 +1,3 @@
-/* $PostgreSQL: pgsql/contrib/pageinspect/uninstall_pageinspect.sql,v 1.5 2009/06/08 16:22:44 tgl Exp $ */
-
 -- Adjust this setting to control where the objects get dropped.
 SET search_path = public;
 
