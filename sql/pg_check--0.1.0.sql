c0a5c16d8b091d1d8e6623b1edb759acb942c2e5
Tomas Vondra <tv@fuzzy.cz>
2012-04-05 18:05:51 +0200

    Added comments to the functions.

--- sql/pg_check--0.1.0.sql
+++ sql/pg_check--0.1.0.sql
@@ -10,10 +10,14 @@ RETURNS int4
 AS '$libdir/pg_check', 'pg_check_table'
 LANGUAGE C STRICT;
 
+COMMENT ON FUNCTION pg_check_table(regclass, bool) IS 'checks consistency of the whole table (and optionally all indexes on it)';
+
 CREATE OR REPLACE FUNCTION pg_check_table(table_relation regclass, block_start bigint, block_end bigint)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_table_pages'
 LANGUAGE C STRICT;
+
+COMMENT ON FUNCTION pg_check_table(regclass, bigint, bigint) IS 'checks consistency of a part of the table (range of pages)';
 
 --
 -- pg_check_index()
@@ -24,7 +28,11 @@ RETURNS int4
 AS '$libdir/pg_check', 'pg_check_index'
 LANGUAGE C STRICT;
 
+COMMENT ON FUNCTION pg_check_index(regclass) IS 'checks consistency of the whole index';
+
 CREATE OR REPLACE FUNCTION pg_check_index(index_relation regclass, block_start bigint, block_end bigint)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_index_pages'
 LANGUAGE C STRICT;
+
+COMMENT ON FUNCTION pg_check_index(regclass, bigint, bigint) IS 'checks consistency of a part of the index (range of pages)';
