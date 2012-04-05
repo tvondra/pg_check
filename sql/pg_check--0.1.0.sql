773d56e9ddc069f721a549798b72afb7b9448d19
Tomas Vondra <tv@fuzzy.cz>
2012-04-05 17:58:27 +0200

    Added parameter names to function definitions.

--- sql/pg_check--0.1.0.sql
+++ sql/pg_check--0.1.0.sql
@@ -5,12 +5,12 @@ SET search_path = public;
 -- pg_check_table()
 --
 
-CREATE OR REPLACE FUNCTION pg_check_table(regclass, bool)
+CREATE OR REPLACE FUNCTION pg_check_table(table_relation regclass, check_indexes bool)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_table'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION pg_check_table(regclass, bigint, bigint)
+CREATE OR REPLACE FUNCTION pg_check_table(table_relation regclass, block_start bigint, block_end bigint)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_table_pages'
 LANGUAGE C STRICT;
@@ -19,12 +19,12 @@ LANGUAGE C STRICT;
 -- pg_check_index()
 --
 
-CREATE OR REPLACE FUNCTION pg_check_index(regclass)
+CREATE OR REPLACE FUNCTION pg_check_index(index_relation regclass)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_index'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION pg_check_index(regclass, bigint, bigint)
+CREATE OR REPLACE FUNCTION pg_check_index(index_relation regclass, block_start bigint, block_end bigint)
 RETURNS int4
 AS '$libdir/pg_check', 'pg_check_index_pages'
 LANGUAGE C STRICT;
